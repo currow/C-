6a6b1b6a751e9508aaf3d23a0468dc16e0a17058
Currow <currops@gmail.com>
2021-02-01 15:32:30 +0100

    [currow] Subida inicial del método principal

---
+++ Naves/pong.c
@@ -0,0 +1,10 @@
+/*
+Juego sencillo del pong:
+ - Hay una pala que se mueve en posición vertical
+ - Una pelota que rebota por las paredes
+ - Un tanteo hasta llegar el final
+*/
+
+int main() {
+    return 0;
+}
